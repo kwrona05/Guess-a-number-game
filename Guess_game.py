7fb2853afc01db91646f171d0877aedb005934bf
kwrona05 <kwrona05@gmail.com>
2024-08-22 06:59:14 +0200

    Add colors to questions and allerts

--- Guess_game.py
+++ Guess_game.py
@@ -10,28 +10,33 @@ def guess():
         {"min": 1, "max": 100}
     ]
 
+    Red = "\033[91m"
+    Green = "\033[92m"
+    Yellow = "\033[93m"
+    Reset = "\033[0m"
+
     for i, level in enumerate(levels, start=1):
         min_value = level["min"]
         max_value = level["max"]
         number_to_guess = random.randint(min_value, max_value)
 
-        print(f'Level {i}: Guess the number between {min_value} and {max_value}')
+        print(f'{Yellow}Level {i}: Guess the number between {min_value} and {max_value}{Reset}')
 
         your_number = None
 
         while your_number != number_to_guess:
             try:
-                your_number = int(input(f'Try to guess the number between {min_value} and {max_value}: '))
+                your_number = int(input(f'{Yellow}Try to guess the number between {min_value} and {max_value}:{Reset} '))
             except ValueError:
-                print('Enter a valid number')
+                print(f'{Red}Enter a valid number{Reset}')
                 continue
             
             if your_number < number_to_guess:
-                print('Too low')
+                print(f'{Red}Too low{Reset}')
             elif your_number > number_to_guess:
-                print('Too high')
+                print(f'{Red}Too high{Reset}')
             else:
-                print(f'Congrats! You passed level {i}')
-    print('Wow! You are master! You passed all levels!')
+                print(f'{Green}Congrats! You passed level {i}{Reset}')
+    print(f'{Green}Wow! You are master! You passed all levels!{Reset}')
 
 guess()
